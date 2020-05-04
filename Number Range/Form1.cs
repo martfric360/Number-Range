cbb57fb82c3a496fdfc94431b3fd9bd3d394abae
Martin Frichitthavong <frichitthavongmartin@gmail.com>
2020-05-04 14:02:54 -0400

    Add project files.

---
+++ Number Range/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Number_Range
+{
+    public partial class Form1 : Form
+    {
+
+        int startNum, lastNum, x; 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Refresh();
+            startNum = Convert.ToInt32(textBox1.Text);
+            lastNum = Convert.ToInt32(textBox2.Text);
+            for (x = startNum; x <= lastNum; x = x + 1)
+            {
+                outputLabel.Text += " " + x; 
+            }
+        }
+    }
+}
